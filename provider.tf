88e9c814411d974bedcdc364451fb5c432351363
MadhuprakashM <madhuprakash912@gmail.com>
2026-05-04 11:18:42 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAZR7JQDJWTWZIR7WJ"
-  secret_key = "eDssphjLn4DN34GbLPUEHsllkXulI5qckqdbPSfU"
+  access_key = "YOUR_ACCESS_KEY"
+  secret_key = "YOUR_SECRET_KEY"
 }
